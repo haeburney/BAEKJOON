46328d40f6163d438e77054787a69a5d578c21bb
haeburney <76997276+haeburney@users.noreply.github.com>
2021-12-28 16:11:49 +0900

    Create Array_2751.java

---
+++ Array_2751.java
@@ -0,0 +1,28 @@
+package baekjoon;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Array_2751 {
+	
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		StringBuilder sb=new StringBuilder();
+		
+		int n=sc.nextInt();
+		
+		ArrayList<Integer> list=new ArrayList<>();
+		
+		for(int i=0;i<n;i++) {
+			list.add(sc.nextInt());
+		}
+		
+		Collections.sort(list);
+		
+		for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		System.out.println(sb);
+	}
+	
+}
