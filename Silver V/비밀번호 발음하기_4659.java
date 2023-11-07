34eb1e3ce020d46c8b18e01c682aef25a4f3b821
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2023-11-07 14:06:55 +0900

    Create 비밀번호 발음하기_4659.java

---
+++ Silver V/비밀번호 발음하기_4659.java
@@ -0,0 +1,68 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            boolean hasVowel = false; // 모음이 있는가
+            boolean hasLessThan3ConsecutiveVandC = true; // 모음 혹은 자음이 3개 미만인가
+            boolean noRepeatingChars = true; // 같은 글자
+            String password = br.readLine();
+
+            if (password.equals("end")) {
+                break;
+            }
+
+            int vowel = 0;
+            int consonant = 0;
+            // 모음 포함
+            for (int i = 0; i < password.length(); i++) {
+                int passwordNum = (int) password.charAt(i);
+                // 모음 혹은 자음이 3개 연속
+                if (passwordNum == 97 || passwordNum == 101 || passwordNum == 105 || passwordNum == 111 || passwordNum == 117) {
+                    hasVowel = true;
+                    vowel++;
+                    consonant = 0;
+                } else {
+                    vowel = 0;
+                    consonant++;
+                }
+
+
+
+                if (vowel >= 3 || consonant >= 3) {
+                    hasLessThan3ConsecutiveVandC = false;
+                    break;
+                }
+
+                if (i < password.length() - 1) {
+                    if (password.charAt(i) == password.charAt(i + 1) && passwordNum != 101 && passwordNum != 111) {
+                        noRepeatingChars = false;
+                    }
+                }
+            }
+
+            if (hasVowel == false || hasLessThan3ConsecutiveVandC == false || noRepeatingChars == false) {
+                bw.write("<" + password + "> is not acceptable.\n");
+            } else {
+                bw.write("<" + password + "> is acceptable.\n");
+            }
+
+            // 같은 글자가 연속적으로 오면 안 됨, 단 ee와 oo 허용
+
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
